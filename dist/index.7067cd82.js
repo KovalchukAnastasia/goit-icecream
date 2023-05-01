c166291c635afdfb6e699e46db901a45c9e8d5c8
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2023-05-01 12:13:26 +0300

    add

---
+++ dist/index.7067cd82.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) document.getElementById("myBtn").style.display = "block";
+    else document.getElementById("myBtn").style.display = "none";
+}
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
+
+//# sourceMappingURL=index.7067cd82.js.map
